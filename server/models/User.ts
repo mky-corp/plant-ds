7245454a155339a0ce21b670337315ef8872a8d1
Franklin Huichi <carloshuichi@gmail.com>
2021-07-31 18:32:46 -0500

    formateando con prettier

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,11 +1,11 @@
-import {Schema, model} from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
-  names: {type: String, required: true},
-  surnames: {type: String, required: true},
-  email: {type: String, unique: true, required: true, lowercase: true},
-  password: {type: String, required: true},
-  state: {type: Boolean, default: true}
+  names: { type: String, required: true },
+  surnames: { type: String, required: true },
+  email: { type: String, unique: true, required: true, lowercase: true },
+  password: { type: String, required: true },
+  state: { type: Boolean, default: true }
 });
 
 export default model('Users', userSchema);
