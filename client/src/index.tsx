1321f84fb3e73ffd03c88661ba958c9eee43ca39
Jochizan <remnyachizot2015@gmail.com>
2021-08-05 17:41:54 -0500

    Config colors and files backend

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import './App.css';
+
+const d = document;
+const $app = d.getElementById('app');
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  $app
 );
 
 // If you want to start measuring performance in your app, pass a function
