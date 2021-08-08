d319edbaaeb8cb2b4308280891c03cc15536fca2
Jochizan <remnyachizot2015@gmail.com>
2021-08-07 23:05:47 -0500

    Fixed project envs

--- server/source/config/config.ts
+++ server/source/config/config.ts
@@ -5,7 +5,7 @@ config();
 export default {
   TOKEN_SECRET: process.env.TOKEN_SECRET + '',
   TOKEN_ISSUER: process.env.TOKEN_ISSUER + '',
-  EXPIRE_TIME: process.env.EXPIRE_TIME || 3600,
-  DB_URL: process.env.DB_URL || 'none',
-  PORT: process.env.PORT || '4000'
+  EXPIRE_TIME: process.env.EXPIRE_TIME + '',
+  DB_URL: process.env.DB_URL + '',
+  PORT: process.env.PORT + ''
 };
