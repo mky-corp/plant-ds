0623a4bf0cdb0d812e64e319ad3eeac5eeeb58d8
Jochizan <remnyachizot2015@gmail.com>
2021-08-19 12:41:05 -0500

    Fixed log config

--- server/source/config/logging.ts
+++ server/source/config/logging.ts
@@ -1,32 +1,56 @@
 export const info = (namespace: string, message: string, object?: any) => {
   if (object) {
-    console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
+    console.info(
+      '[%s] [INFO] [%s] %s',
+      getTimeStamp(),
+      namespace,
+      message,
+      object
+    );
   } else {
-    console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
+    console.info('[%s] [INFO] [%s] %s', getTimeStamp(), namespace, message);
   }
 };
 
 export const warn = (namespace: string, message: string, object?: any) => {
   if (object) {
-    console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`, object);
+    console.warn(
+      '[%s] [WARN] [%s] %s',
+      getTimeStamp(),
+      namespace,
+      message,
+      object
+    );
   } else {
-    console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
+    console.warn('[%s] [WARN] [%s] %s', getTimeStamp(), namespace, message);
   }
 };
 
 export const error = (namespace: string, message: string, object?: any) => {
   if (object) {
-    console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object);
+    console.error(
+      '[%s] [ERROR] [%s] %s',
+      getTimeStamp(),
+      namespace,
+      message,
+      object
+    );
   } else {
-    console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
+    console.error('[%s] [ERROR] [%s] %s', getTimeStamp(), namespace, message);
   }
 };
 
 export const debug = (namespace: string, message: string, object?: any) => {
   if (object) {
-    console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`, object);
+    console.debug(
+      '[%s] [DEBUG] [%s] %s',
+      getTimeStamp(),
+      namespace,
+      message,
+      object
+    );
   } else {
-    console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
+    console.debug('[%s] [DEBUG] [%s] %s', getTimeStamp(), namespace, message);
   }
 };
 
