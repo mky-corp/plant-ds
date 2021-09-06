852b3172c51b3a52f6164b52ba62c40944e472b1
Jochizan <remnyachizot2015@gmail.com>
2021-09-06 17:59:49 -0500

    Estableciendo proyecto solo con create-react-app

--- server/source/controllers/index.controllers.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const viewIndex = (req: Request, res: Response, next: NextFunction) => {
-  res.render('index', { title: 'Express' });
-};
--- server/source/routes/index.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { viewIndex } from '../controllers/index.controllers';
-
-const router = Router();
-
-/* GET home page. */
-router.get('/', viewIndex);
-
-export default router;
